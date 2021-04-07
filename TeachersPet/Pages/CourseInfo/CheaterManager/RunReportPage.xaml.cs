3e42bcb7286b3c8716c1be79889c0108382de21d
tgoody <goodmane@ufl.edu>
2021-04-07 18:21:46 -0400

    fix pt 2

--- TeachersPet/Pages/CourseInfo/CheaterManager/RunReportPage.xaml.cs
+++ TeachersPet/Pages/CourseInfo/CheaterManager/RunReportPage.xaml.cs
@@ -131,12 +131,12 @@ namespace TeachersPet.Pages.CourseInfo.CheaterManager {
                 Task.Run(() => {
                     resultHyperLink = MossReportService.RunReportOnDirectory(_workingDirectory, language, numSimilarLines,
                         numReports, nameOfReport, excludedFiles).Trim();
-                    if (string.IsNullOrEmpty(resultHyperLink)) {
-                        throw new Exception("No link available for MOSS");
-                    }
+                    
                     Dispatcher.Invoke(() => {
                         LoadingTextBlock.Visibility = Visibility.Collapsed;
-                        ResultsButton.Visibility = Visibility.Visible;
+                        if (!string.IsNullOrEmpty(resultHyperLink)) {
+                            ResultsButton.Visibility = Visibility.Visible;
+                        }
                     });
                 });
                 LoadingTextBlock.Visibility = Visibility.Visible;
